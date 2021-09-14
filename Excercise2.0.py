fa3056871bd677b2e902af30fdd85760f4548afc
cullimored <90724322+cullimored@users.noreply.github.com>
2021-09-14 16:59:43 -0400

    Add files via upload

---
+++ Excercise2.0.py
@@ -0,0 +1,4 @@
+x=("darius, 9-23-14, at what size of a project should you start using version management software?")
+y=("version management software is usually when you get to big projects. These sizable projects include corporate code blocks in which multiple people develop their different programs and software that help run the companies processes." )
+xz=(x+y)
+print("answer:",xz)
