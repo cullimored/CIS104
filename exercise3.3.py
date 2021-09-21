b3c823c904c47c31fffe7983286b1fbb3459f550
cullimored <90724322+cullimored@users.noreply.github.com>
2021-09-21 17:17:04 -0400

    Add files via upload

---
+++ exercise3.3.py
@@ -0,0 +1,11 @@
+score = input("enter score: ")
+score = float(score)
+if score >=.9:
+    print("A")
+elif score >=.8:
+    print("B")
+elif score >=.7:
+    print("C")
+elif score >=.6:
+    print("D")
+else: print("bad grade")
